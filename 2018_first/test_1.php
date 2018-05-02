183493650b88c3ee7079aadd398f226c899fbf5e
DESKTOP-8B2HOEB\kosuke.shinke <shinke.kousuke@no1s.biz>
2018-05-02 18:37:19 +0900

    2018年第一回 統一プログラミングテスト

---
+++ 2018_first/test_1.php
@@ -0,0 +1,159 @@
+<?php
+
+$searchCount = 10;
+$twitterPictures = 0;
+
+try{
+    // ディレクトリ作成
+    $directory_path = "../tmp";
+    if(!file_exists($directory_path)){
+        if(!mkdir($directory_path, 0777)){
+            throw new Exception('ディレクトリの作成に失敗しました。');
+        }
+    }
+
+    // 画像取得
+    while(count($twitterPictures) < 10){
+        $twitterPictures = getTwitterInfo($searchCount);
+        $searchCount = $searchCount + 5; 
+    }
+
+    // 取得した画像ファイルを保存
+    if(count($twitterPictures) > 0){
+        foreach($twitterPictures as $twitterPicture){
+            $data = file_get_contents($twitterPicture);
+            $fileName = basename($twitterPicture);
+            file_put_contents($directory_path.'/'.$fileName, $data);
+        }
+     }else {
+       echo "対象の画像が存在しませんでした。";
+     }
+     
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+
+// ツイッタの画像を検索
+function getTwitterInfo($count){
+    $url = array();
+    
+    // 設定
+    $api_key = '' ;        // APIキー
+    $api_secret = '' ;        // APIシークレット
+    $access_token = '' ;        // アクセストークン
+    $access_token_secret = '' ;        // アクセストークンシークレット
+    $request_url = 'https://api.twitter.com/1.1/search/tweets.json' ; 
+    $request_method = 'GET' ;
+    
+    // パラメータA (オプション)
+    $params_a = array(
+        "q" => "JustinBieber",
+        "count" => $count,
+    ) ;
+
+    // キーを作成する (URLエンコードする)
+    $signature_key = rawurlencode( $api_secret ) . '&' . rawurlencode( $access_token_secret ) ;
+
+    // パラメータB (署名の材料用)
+    $params_b = array(
+        'oauth_token' => $access_token ,
+        'oauth_consumer_key' => $api_key ,
+        'oauth_signature_method' => 'HMAC-SHA1' ,
+        'oauth_timestamp' => time() ,
+        'oauth_nonce' => microtime() ,
+        'oauth_version' => '1.0' ,
+    ) ;
+
+    // パラメータAとパラメータBを合成してパラメータCを作る
+    $params_c = array_merge( $params_a , $params_b ) ;
+
+    // 連想配列をアルファベット順に並び替える
+    ksort( $params_c ) ;
+
+    // パラメータの連想配列を[キー=値&キー=値...]の文字列に変換する
+    $request_params = http_build_query( $params_c , '' , '&' ) ;
+
+    // 一部の文字列をフォロー
+    $request_params = str_replace( array( '+' , '%7E' ) , array( '%20' , '~' ) , $request_params ) ;
+
+    // 変換した文字列をURLエンコードする
+    $request_params = rawurlencode( $request_params ) ;
+
+    // リクエストメソッドをURLエンコードする
+    // ここでは、URL末尾の[?]以下は付けないこと
+    $encoded_request_method = rawurlencode( $request_method ) ;
+
+    // リクエストURLをURLエンコードする
+    $encoded_request_url = rawurlencode( $request_url ) ;
+
+    // リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
+    $signature_data = $encoded_request_method . '&' . $encoded_request_url . '&' . $request_params ;
+
+    // キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
+    $hash = hash_hmac( 'sha1' , $signature_data , $signature_key , TRUE ) ;
+
+    // base64エンコードして、署名[$signature]が完成する
+    $signature = base64_encode( $hash ) ;
+
+    // パラメータの連想配列、[$params]に、作成した署名を加える
+    $params_c['oauth_signature'] = $signature ;
+
+    // パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
+    $header_params = http_build_query( $params_c , '' , ',' ) ;
+
+    // リクエスト用のコンテキスト
+    $context = array(
+        'http' => array(
+            'method' => $request_method , // リクエストメソッド
+            'header' => array(              // ヘッダー
+                'Authorization: OAuth ' . $header_params ,
+            ) ,
+        ) ,
+    ) ;
+
+    // パラメータがある場合、URLの末尾に追加
+    if( $params_a ) {
+        $request_url .= '?' . http_build_query( $params_a ) ;
+    }
+
+    // cURLを使ってリクエスト
+    $curl = curl_init() ;
+    curl_setopt( $curl, CURLOPT_URL , $request_url ) ;
+    curl_setopt( $curl, CURLOPT_HEADER, 1 ) ; 
+    curl_setopt( $curl, CURLOPT_CUSTOMREQUEST , $context['http']['method'] ) ;    // メソッド
+    curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER , false ) ;    // 証明書の検証を行わない
+    curl_setopt( $curl, CURLOPT_RETURNTRANSFER , true ) ;    // curl_execの結果を文字列で返す
+    curl_setopt( $curl, CURLOPT_HTTPHEADER , $context['http']['header'] ) ;    // ヘッダー
+    curl_setopt( $curl , CURLOPT_TIMEOUT , 30 ) ;    // タイムアウトの秒数
+
+    $res1 = curl_exec( $curl ) ;
+    $res2 = curl_getinfo( $curl ) ;
+    curl_close( $curl ) ;
+
+    // 取得したデータ
+    $json = substr( $res1, $res2['header_size'] ) ;        // 取得したデータ(JSONなど)
+    $header = substr( $res1, 0, $res2['header_size'] ) ;    // レスポンスヘッダー (検証に利用したい場合にどうぞ)
+
+
+    // JSONをオブジェクトに変換
+    $obj = json_decode( $json ) ;
+    if(isset($obj->errors)){
+        throw new Exception($obj->errors[0]->message);
+    }
+    if(!isset($obj->statuses)){
+        return array();
+    }
+    
+    foreach($obj->statuses as $key=>$twit){
+        if(isset($twit->entities->media)){
+            $url[] = $twit->entities->media[0]->media_url;
+            if(count($url) >= 10){
+               break;
+            }
+        }
+    }
+    
+    return $url;
+}
+
+?>
