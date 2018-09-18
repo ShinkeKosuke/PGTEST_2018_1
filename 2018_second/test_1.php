1b407a7abf2cd07da95fa43446cd0a8cd9e441b1
DESKTOP-8B2HOEB\kosuke.shinke <shinke.kousuke@no1s.biz>
2018-09-18 20:57:43 +0900

    第二回　統一プログラミングテスト

--- 2018_second/test_1.php
+++ 2018_second/test_1.php
@@ -1,30 +1,31 @@
 <?php
 
 // APIキー
-$api = "AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA";
+$api = "";
 
 $spreadsheetsUrl = "https://docs.google.com/spreadsheets/d/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/edit#gid=0";
 
 // スプレットシートIDの取得
-$w = preg_quote("/spreadsheets¥/d¥/([a-zA-Z0-9-_]+)", "/");
-preg_match($w, $spreadsheetsUrl, $spreadsheetId);
+$spreadsheetId = str_replace('/edit#gid=0','',str_replace('https://docs.google.com/spreadsheets/d/', '', $spreadsheetsUrl));
 
-var_dump($spreadsheetId);
 // 送信先
 $url = "https://sheets.googleapis.com/v4/spreadsheets/".$spreadsheetId."/values/A1:E6?key=".$api;
 
 try{
     // レスポンスの取得
     $response = file_get_contents($url);
-
-    $a=json_decode($response);
-    foreach($a->values as $value){
-        foreach($value as $e){
-           echo "'".mb_convert_encoding($e, "SJIS")."',";
-      }
-      echo "\n";
+    if($response){
+        $response_decode =json_decode($response);
+        foreach($response_decode->values as $values){
+            foreach($values as $value){
+               echo "'".mb_convert_encoding($value, "SJIS")."',";
+            }
+            echo "\n";
+        }
+    } else {
+        throw new Exception('ディレクトリの作成に失敗しました。');
     }
 } catch (Exception $e) {
-    echo "取得に失敗しました。";
+    echo $e->getMessage(), "\n";
 }
 ?>
--- 2018_second/test_2/test2.js
+++ 2018_second/test_2/test2.js
@@ -1,3 +1,97 @@
 document.onkeydown = keydown ; // イベント検出
 document.onkeyup   = keyup ; // イベント検出
 
+// 画像の表示座標・移動量
+var x  = 400 ; // 表示する座標
+var pv =  50 ; // 1回の移動量
+
+// キーボードのフラグ　（1 : 押されている / 0 : 離されている）
+var left  = 0 ;
+var right = 0 ;
+var image = "";
+
+//画像を配列に格納する
+var img_left = new Array();
+var img_right = new Array();
+
+img_right[0] = new Image();
+img_right[0].src = "images/mario_1.png";
+img_right[1] = new Image();
+img_right[1].src = "images/mario_2.png";
+img_right[2] = new Image();
+img_right[2].src=  "images/mario_3.png";
+
+img_left[0] = new Image();
+img_left[0].src = "images/mario_4.png";
+img_left[1] = new Image();
+img_left[1].src = "images/mario_5.png";
+img_left[2] = new Image();
+img_left[2].src=  "images/mario_6.png";
+
+//画像番号用のグローバル変数
+var cnt=0;
+
+/********** キーボードのイベント（押された） **********/
+
+function keydown(event) // 押されたキーのフラグを　１　にする
+{
+  if(event.keyCode == 37) //left key
+  {
+    left = 1;
+    image = "left";
+  }
+  if(event.keyCode == 39) //right key
+  {
+    right = 1;
+    image = "right";
+  }
+  changeIMG();
+  disp() ;
+}
+
+function disp() // キーボード入力を反映して表示座標を更新
+{
+  if(left==1)   x = x - pv;
+  if(right==1)  x = x + pv;
+
+  if(x < 0){
+    x = 0;
+  }else if(x > 1050){
+    x = 1000;
+  }else{
+    document.getElementById("image").style.left = x;
+  }
+}
+
+//画像切り替え関数
+function changeIMG(){
+  //画像番号を進める
+  if (cnt == 2)
+  { cnt=0; }
+  else
+  { cnt++; }
+  
+  //画像を切り替える
+  if(image == "left"){
+      document.getElementById("image").src=img_left[cnt].src;
+  }
+  if(image == "right"){
+      document.getElementById("image").src=img_right[cnt].src;
+  } 
+}
+
+
+/********** キーボードのイベント（離された） **********/
+
+function keyup(event) // 離されたキーのフラグを　０　にする
+{
+  if(event.keyCode == 37) //left key
+  {
+    left = 0;
+  }
+  if(event.keyCode == 39) //right key
+  {
+    right = 0;
+  }
+  image = "";
+}
